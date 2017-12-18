17065cc055970b4b3e2bb4d16fa42021e37e5c08
Peter Bui <pbui@bx612.space>
2017-12-18 17:29:28 -0500

    scripts: update to python3

--- scripts/load_presets.py
+++ scripts/load_presets.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
+
+import os
+import sys
+
+sys.path.append(os.curdir)
 
 import sqlite3
 import yldme
@@ -7,7 +12,7 @@ db = yldme.Database()
 
 for name, value, type in yldme.YLDME_PRESETS:
     try:
-        print name, value, type
+        print(name, value, type)
         db.add(name, value, type)
     except sqlite3.IntegrityError:
         pass
