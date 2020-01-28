b6aa19c2e6b15d94d329957b1e45af36e3743ae7
マイケル・アイゼマン <japanese_kitkat@yahoo.co.jp>
2020-01-28 16:44:03 -0500

    remove bad code

--- yldme.py
+++ yldme.py
@@ -379,14 +379,14 @@ class YldMeHandler(tornado.web.RequestHandler):
             # self.application.logger.info('Posted: {}'.format(raw_url))
 
             # convert mov to mp4
-            if 'quicktime' in file_mime:
-                os.rename('/home/akk/www/uploads/' + data.name, '/home/akk/www/uploads/' + data.name + '.temp')
-                convert = ['ffmpeg', '-i', '/home/akk/www/uploads/' + data.name + '.temp', '-f', 'mp4', '/home/akk/www/uploads/' + data.name]
-                # move = ['mv', '/home/akk/www/uploads/out.mp4', '/home/akk/www/uploads/' + data.name]
-                reee = tornado.process.Subprocess(convert)
-                # os.rename('/home/akk/www/uploads/' + data.name + '.mp4', '/home/akk/www/uploads' + data.name)
-                file_mime = 'video/mp4'
-                file_ext = '.mp4'
+            # if 'quicktime' in file_mime:
+            #     os.rename('/home/akk/www/uploads/' + data.name, '/home/akk/www/uploads/' + data.name + '.temp')
+            #     convert = ['ffmpeg', '-i', '/home/akk/www/uploads/' + data.name + '.temp', '-f', 'mp4', '/home/akk/www/uploads/' + data.name]
+            #     # move = ['mv', '/home/akk/www/uploads/out.mp4', '/home/akk/www/uploads/' + data.name]
+            #     reee = tornado.process.Subprocess(convert)
+            #     # os.rename('/home/akk/www/uploads/' + data.name + '.mp4', '/home/akk/www/uploads' + data.name)
+            #     file_mime = 'video/mp4'
+            #     file_ext = '.mp4'
 
             raw_url   = '{}/raw/{}{}'.format(self.application.url, data.name, file_ext)
             self.application.logger.info('Posted: {}'.format(raw_url))
